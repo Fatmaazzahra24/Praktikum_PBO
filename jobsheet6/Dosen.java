60d9669e94c30b50d9c2635fa298bed4220a6646
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2025-10-03 10:30:30 +0700

    jobsheet 6

---
+++ jobsheet6/Dosen.java
@@ -0,0 +1,19 @@
+public class Dosen extends Pegawai {
+    public String nidn;
+
+    public Dosen(String nip, String nama, double gaji, String nidn) {
+        super(nip, nama, gaji); 
+        this.nidn = nidn;      
+    }
+
+    @Override
+    public String getInfo() {
+        return "NIDN : " + this.nidn + "\n";
+    }
+
+    public String getAllInfo() {
+        String info = super.getInfo();
+        info += "NIDN : " + nidn + "\n";
+        return info;
+    }
+}
---
+++ jobsheet6/inharitanceDemo.java
@@ -0,0 +1,6 @@
+public class inharitanceDemo {
+    public static void main(String[] args) {
+        Dosen dosen2 = new Dosen("34329837", "Yansy Ayuningtyas", 3000000.0, "1989432439");
+        System.out.println(dosen2.getAllInfo());
+    }
+}
