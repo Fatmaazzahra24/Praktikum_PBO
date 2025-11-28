b2f3b743ea23d3461bde7a05edd18a65414d16fc
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2025-11-28 09:37:26 +0700

    jobsheet 11 polimorfis

---
+++ jobsheet11/Demo.java
@@ -0,0 +1,19 @@
+public class Demo {
+    public static void main(String[] args) {
+
+        Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.Kom", "199402");
+        TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", "Aida, A.Md.", "Tenaga Administrasi");
+       
+        train(dosen1);
+        train(tendik1);
+
+    }
+    public static void train(Pegawai pegawai) {
+        System.out.println("Memberikan pelatihan untuk pegawaai:");
+        pegawai.displayInfo();
+
+        if (pegawai instanceof Dosen) {
+           System.out.println("Memberikan pelatihan pedagogik");
+        }
+    }
+}
